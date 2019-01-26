37d9a686020811d5c5884df27e574fb1fcec7108
MiChel Causey <michel.causey@gmail.com>
2019-01-26 08:57:47 -0500

    nope

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,6 @@ router.get("/books", (req, res) => {
   .get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}&key=AIzaSyCg2gkORQQTZ-24DleOxpUUj3Zp-5bbCgE`)
   .then(({ data: { results } }) => res.json(results))
   .catch(err => res.status(422).json(err))
-  console.log(results)
 });
 
 module.exports = router;
