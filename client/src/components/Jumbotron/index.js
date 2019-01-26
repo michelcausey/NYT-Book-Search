52eff82a220be1b8ca9a027ea966c187871854f3
MiChel Causey <michel.causey@gmail.com>
2019-01-25 21:39:50 -0500

    this does not work

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,10 +4,7 @@ import "./style.css";
 function Jumbotron() {
   return (
     <div className="jumbotron text-center">
-      <h1>NYT Google Books Search</h1>
-      <a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/">
-        Search for your favorite book, and save it for later
-      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/"><h1>NYT Google Books Search</h1></a>
     </div>
   );
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-const stringify = require('json-stringify-safe')
+// const stringify = require('json-stringify-safe')
 
 router.get("/books", (req, res) => {
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}&key=AIzaSyCg2gkORQQTZ-24DleOxpUUj3Zp-5bbCgE`)
-  .then(results => res.json(results.data.items))
+  axios
+  .get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}&key=AIzaSyCg2gkORQQTZ-24DleOxpUUj3Zp-5bbCgE`)
+  .then(res => res.json(results.data.items))
   .catch(err => res.status(422).json(err));
 });
 
